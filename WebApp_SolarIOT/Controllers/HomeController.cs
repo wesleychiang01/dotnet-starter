b992151b58eb32fd5d147b07b19d9536223b6706
wesleyhiang96 <wesleychiang96@gmail.com>
2024-01-19 19:46:52 +0800

    update

--- WebApp_SolarIOT/Controllers/HomeController.cs
+++ WebApp_SolarIOT/Controllers/HomeController.cs
@@ -1,25 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Reflection;
+using WebApp_SolarIOT.Areas.Identity.Data;
+using WebApp_SolarIOT.Data;
 using WebApp_SolarIOT.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace WebApp_SolarIOT.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+            this._userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var data = await _context.parameters
+
+           .OrderByDescending(p => p.EventProcessedUtcTime)
+           .Select(p => new { p.battery_volt, p.solar_amp, p.temperature, p.light_intensity, p.EventProcessedUtcTime })
+           .Take(10)
+           .ToListAsync();
+
+            var batteryVoltData = data.Select(d => d.battery_volt).ToArray();
+            var solarAmpData = data.Select(d => d.solar_amp).ToArray();
+            var temperatureData = data.Select(d => d.temperature).ToArray();
+            var lightIntensityData = data.Select(d => d.light_intensity).ToArray();
+            var eventTimeData = data.Select(d => d.EventProcessedUtcTime.ToString("MM/dd/yyyy HH:mm:ss")).ToArray();
+
+            Array.Reverse(batteryVoltData);
+            Array.Reverse(solarAmpData);
+            Array.Reverse(temperatureData);
+            Array.Reverse(lightIntensityData);
+            Array.Reverse(eventTimeData);
+
+            ViewBag.BatteryVoltData = batteryVoltData;
+            ViewBag.SolarAmpData = solarAmpData;
+            ViewBag.TemperatureData = temperatureData;
+            ViewBag.LightIntensityData = lightIntensityData;
+            ViewBag.EventTimeData = eventTimeData;
+
+            var latestParameters = await _context.parameters
+               .OrderByDescending(p => p.EventProcessedUtcTime)
+               .FirstOrDefaultAsync();
+
+            if (latestParameters == null)
+            {
+                return NotFound("No latest parameters found.");
+            }
+
+            return View(latestParameters);
         }
 
         public IActionResult Privacy()
         {
+            ViewData["UserID"]= _userManager.GetUserId(this.User);
             return View();
         }
 
--- WebApp_SolarIOT/Program.cs
+++ WebApp_SolarIOT/Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp_SolarIOT.Data;
+using Microsoft.AspNetCore.Identity;
+using WebApp_SolarIOT.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -12,6 +22,16 @@ sqlServerOptionsAction: sqlOptions =>
 {
     sqlOptions.EnableRetryOnFailure();
 }));
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>>();
 
 var app = builder.Build();
 
@@ -33,5 +53,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
